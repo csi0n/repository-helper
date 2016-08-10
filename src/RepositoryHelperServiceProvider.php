a7a95deea3dc29c9643c37a5f1b2e06feb48facd
huaqing chen <csi0n@csi0n.local>
2016-08-10 20:27:54 +0800

    测试

---
+++ src/RepositoryHelperServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+namespace csi0n\LaravelIdeHelper;
+
+use Illuminate\Support\ServiceProvider;
+
+class RepositoryHelperServiceProvider extends ServiceProvider
+{
+    public $defer = true;
+
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+
+}
